cc2dcddbc307b78a8c63daa2ba35afe003266839
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-09-30 21:24:43 +0300

    add some options.

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var system = require('./system/main');
 var routes = require('./routes/main');
 
-app = system.init();
+app = system.init({
+	default_routes: true,
+	middleware: true,
+	sessions: true,
+	staff_mode: true,
+	logger: true,
+	validator: true,
+	pjax: true,
+	template_dir: __dirname+"/templates",
+	public_dir: __dirname+"/public",
+	view_engine: "jade",
+});
 
 app.set('title', 'Hype');
 
