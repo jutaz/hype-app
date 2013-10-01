e4467875a9316467c78052eb24c279f0b9bf1e52
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-10-01 14:21:16 +0300

    update to support hype npm module

--- system/error_pages.js
+++
@@ -1,9 +0,0 @@
-var middleware = require("./lib/middleware");
-var routes = require('./routes/main');
-
-module.exports = function(app) {
-	app.post('/log/error', routes.log_error);
-	app.all('*', routes.error_404);
-	app.use(middleware.serverError);
-	return app;
-}
--- system/lib/middleware/pjax.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(req, res, next) {
-	if(req.headers["x-pjax"]) {
-		res.locals.pjax = true;
-	} else {
-		res.locals.pjax = false;
-	}
-	next();
-}
